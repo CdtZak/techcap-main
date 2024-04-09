41b82ba4bc8d30b36b9598d7cbe99b80946fd576
CdtZak <zaki0galaxy@gmail.com>
2024-04-09 17:28:40 +0100

    token

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,6 +1,9 @@
 const admin = require('../models/adminModel')
 const order= require('../models/orderModel')
 const pc  = require('../models/pcModel')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken');
+
 exports.getAdmin =  async(req,res,next)=>{
     try{
         const pcs = await pc.find()
@@ -27,5 +30,77 @@ exports.deleteProduct = async (req, res, next) => {
     } catch (err) {
         console.error(err);
         res.status(500).json({ message: "An error occurred while deleting the product" });
+    }
+};
+exports.createAdmin = async (req,res,next) =>{
+    const { username, password } = req.body;
+
+    try {
+       
+        const existingAdmin = await admin.findOne({ username });
+        if (existingAdmin) {
+            return res.status(400).json({ message: 'Admin with this username already exists' });
+        }
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const newAdmin = new admin({ username, password: hashedPassword });
+        await newAdmin.save();
+
+        res.status(201).json({ message: 'Admin created successfully' });
+    } catch (error) {
+        console.error('Error creating admin:', error);
+        res.status(500).json({ message: 'Error creating admin' });
+    }
+
+}
+exports.login = async (req,res,next) =>{
+    res.render('login')
+}
+exports.auth = async (req,res,next)=>{
+    
+    const { username, password } = req.body;
+
+    try {
+        
+        const Admin = await admin.findOne({ username });
+        if (!Admin) {
+            return res.status(404).send('Admin not found');
+        }
+
+        
+        const passwordMatch = await bcrypt.compare(password, Admin.password);
+        if (!passwordMatch) {
+            return res.status(401).send('Invalid password');
+        }
+
+        
+        const token = jwt.sign({ userId: Admin._id }, 'secret-key', { expiresIn: '1h' });
+        console.log(req.headers['authorization'])
+        res.json({ token });
+    } catch (error) {
+        res.status(500).send('Error logging in');
+    }
+
+
+}
+exports.protect = (req, res, next) => {
+console.log(req.headers)
+    const token = req.headers["authorization"]
+
+    
+    if (!token) {
+        return res.status(401).json({ message: 'Access denied. No token provided.' });
+    }
+
+    try {
+        
+        const decodedToken = jwt.verify(token, 'secret-key');
+
+       
+        req.userId = decodedToken.userId;
+
+       
+        next();
+    } catch (error) {
+        return res.status(403).json({ message: 'Invalid token.' });
     }
 };
--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -4,7 +4,10 @@ const adminController = require('./../controllers/adminController')
 
 const router = express.Router()
 
-router.get('/admin',adminController.getAdmin)
+router.get('/admin',adminController.protect,adminController.getAdmin)
 router.delete('/deleteProduct',adminController.deleteProduct)
+router.post('/createAdmin',adminController.createAdmin)
+router.get('/login',adminController.login)
+router.post('/auth',adminController.auth)
 
 module.exports=router
